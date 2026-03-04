a7eb74d3b2ab629a9a2d2edb6d8cd3439ad633df
Aditya-Kumar25 <adityaashu.dynmo@gmail.com>
2026-03-04 23:29:57 +0530

    packages update 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-"use client";
+import { OrganizationSwitcher,UserButton } from "@clerk/nextjs";
 
-import { Button } from "@/components/ui/button";
-import {toast} from "sonner";
-
-export default function Home() {
-  return (
-    <Button onClick={()=> toast.success("Hello World!!")}>
-          Click me!!!
-    </Button>  
-  );
+export default function Home(){
+    return(
+      <div className=" flex min-h-screen flex-col items-center justify-center gap-4 bg-background">
+          <h1 className="text-2xl font-semibold">Welcome to Frequency</h1>
+          <div className="flex items-center gap-4">
+              <OrganizationSwitcher/>
+              <UserButton></UserButton>
+          </div>
+      </div>
+    )
 }
